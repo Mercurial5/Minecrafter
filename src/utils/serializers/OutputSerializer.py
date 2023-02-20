579f07079d9b4c35b69018e0d5d86f515f8d0745
Mercurial5 <dias.nespayev@gmail.com>
2023-02-20 15:47:19 +0600

    fix: change type

--- src/utils/serializers/OutputSerializer.py
+++ src/utils/serializers/OutputSerializer.py
@@ -1,16 +1,17 @@
 from inspect import getmembers
 
 from utils.serializers.fields import BaseField
+from mongoengine import Document
 
 
 class OutputSerializer:
 
-    def __init__(self, validated_data: dict):
+    def __init__(self, validated_data: Document):
         self.fields = {field[0]: field[1] for field in getmembers(self) if isinstance(field[1], BaseField)}
         self.validated_data = validated_data
 
-    def prepare_output(self) -> dict | list:
-        if isinstance(self.validated_data, dict):
+    def prepare_output(self) -> dict | list[dict]:
+        if isinstance(self.validated_data, Document):
             return self._prepare_item_output(self.validated_data)
 
         return self._prepare_list_output()
@@ -18,7 +19,7 @@ class OutputSerializer:
     def _prepare_list_output(self) -> list[dict]:
         return [self._prepare_item_output(data) for data in self.validated_data]
 
-    def _prepare_item_output(self, validated_data: dict) -> dict:
+    def _prepare_item_output(self, validated_data: Document) -> dict:
         output_data = dict()
 
         field_class: BaseField
