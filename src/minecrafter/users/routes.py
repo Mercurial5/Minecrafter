f7073c562c0413cbbeb5a2ac7d3a85b9af29f01c
Mercurial5 <dias.nespayev@gmail.com>
2023-02-20 15:51:21 +0600

    feat: list_users

--- src/minecrafter/users/routes.py
+++ src/minecrafter/users/routes.py
@@ -12,8 +12,17 @@ from minecrafter.users.services import UserService
 from utils.decorators import serializable
 
 
-@app.route('/', methods=['POST'])
-@serializable(request, UserCreateSerializer, UserOutputSerializer)
+@app.route('/', endpoint='list_users', methods=['GET'])
+@serializable(request, output_serializer_class=UserOutputSerializer)
+def list_users():
+    service = UserService()
+    users = service.get_users()
+
+    return users, HTTPStatus.OK
+
+
+@app.route('/', endpoint='create_user', methods=['POST'])
+@serializable(request, input_serializer_class=UserCreateSerializer, output_serializer_class=UserOutputSerializer)
 def create_user(data: dict) -> tuple[Union[dict, User], int]:
     service = UserService()
 
