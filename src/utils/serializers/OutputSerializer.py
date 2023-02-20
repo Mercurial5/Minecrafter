4970eb2ce8f56b5188abb55c11524c73a37d8b4d
Mercurial5 <dias.nespayev@gmail.com>
2023-02-20 15:42:41 +0600

    feat: OutputSerializer prepare many items to output

--- src/utils/serializers/OutputSerializer.py
+++ src/utils/serializers/OutputSerializer.py
@@ -9,11 +9,20 @@ class OutputSerializer:
         self.fields = {field[0]: field[1] for field in getmembers(self) if isinstance(field[1], BaseField)}
         self.validated_data = validated_data
 
-    def prepare_output(self) -> dict:
+    def prepare_output(self) -> dict | list:
+        if isinstance(self.validated_data, dict):
+            return self._prepare_item_output(self.validated_data)
+
+        return self._prepare_list_output()
+
+    def _prepare_list_output(self) -> list[dict]:
+        return [self._prepare_item_output(data) for data in self.validated_data]
+
+    def _prepare_item_output(self, validated_data: dict) -> dict:
         output_data = dict()
 
         field_class: BaseField
         for field_name, field_class in self.fields.items():
-            output_data[field_name] = field_class.cast(self.validated_data[field_name], output=True)
+            output_data[field_name] = field_class.cast(validated_data[field_name], output=True)
 
         return output_data
